2e23b127d5b6b4890e6b64705cf78ea4e0c85408
ddiffa <ddiffa2@gmail.com>
2020-05-13 06:58:49 +0700

    Challenge: Display Starting Score

--- app/src/main/java/com/hellodiffa/yourfirstkotlinandroidapp/MainActivity.kt
+++ app/src/main/java/com/hellodiffa/yourfirstkotlinandroidapp/MainActivity.kt
@@ -1,14 +1,16 @@
 package com.hellodiffa.yourfirstkotlinandroidapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
-    internal lateinit var tapMeButton : Button
-    internal lateinit var gameScoreTextView : TextView
+    internal var score = 0
+
+    internal lateinit var tapMeButton: Button
+    internal lateinit var gameScoreTextView: TextView
     internal lateinit var timeLeftTextView: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,5 +20,16 @@ class MainActivity : AppCompatActivity() {
         gameScoreTextView = findViewById(R.id.gameScoreTextView)
         timeLeftTextView = findViewById(R.id.timeLeftTextView)
 
+        tapMeButton.setOnClickListener { _ ->
+            incrementScore()
+        }
+
+        gameScoreTextView.text = getString(R.string.yourScore, score)
+    }
+
+    private fun incrementScore() {
+        score += 1
+        val newScore = getString(R.string.yourScore, score)
+        gameScoreTextView.text = newScore
     }
 }
