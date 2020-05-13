52c40bb3ad6bbea65f59604763bf420af4762607
ddiffa <ddiffa2@gmail.com>
2020-05-13 07:15:59 +0700

    Starting and Ending the Game

--- app/src/main/java/com/hellodiffa/yourfirstkotlinandroidapp/MainActivity.kt
+++ app/src/main/java/com/hellodiffa/yourfirstkotlinandroidapp/MainActivity.kt
@@ -1,21 +1,35 @@
 package com.hellodiffa.yourfirstkotlinandroidapp
 
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.util.Log
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
     internal var score = 0
 
+    internal var gameStarted = false
+
+    internal lateinit var countDownTimer: CountDownTimer
+    internal val initialCountDown: Long = 60_000L
+    internal val countDownInterval: Long = 1_000L
+
     internal lateinit var tapMeButton: Button
     internal lateinit var gameScoreTextView: TextView
     internal lateinit var timeLeftTextView: TextView
 
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
         tapMeButton = findViewById(R.id.tapMeButton)
         gameScoreTextView = findViewById(R.id.gameScoreTextView)
         timeLeftTextView = findViewById(R.id.timeLeftTextView)
@@ -24,12 +38,51 @@ class MainActivity : AppCompatActivity() {
             incrementScore()
         }
 
+        resetGame()
+    }
+
+    private fun resetGame() {
+        score = 0
+
         gameScoreTextView.text = getString(R.string.yourScore, score)
+
+        val initialTimeLeft = initialCountDown / 1_000L
+        timeLeftTextView.text = getString(R.string.timeLeft, initialTimeLeft)
+
+        countDownTimer = object : CountDownTimer(initialCountDown, countDownInterval) {
+            override fun onFinish() {
+                endGame()
+            }
+
+            override fun onTick(millisUntilFinished: Long) {
+                val timeLeft = millisUntilFinished / 1_000L
+                timeLeftTextView.text = getString(R.string.timeLeft, timeLeft)
+            }
+
+        }
+
+        gameStarted = false
     }
 
     private fun incrementScore() {
+
+        if (!gameStarted) {
+            startGame()
+        }
+
         score += 1
         val newScore = getString(R.string.yourScore, score)
         gameScoreTextView.text = newScore
     }
+
+    private fun startGame() {
+        countDownTimer.start()
+        gameStarted = true
+    }
+
+    private fun endGame() {
+        Toast.makeText(this, getString(R.string.gameOverMessage, score), Toast.LENGTH_LONG).show()
+        resetGame()
+    }
+
 }
